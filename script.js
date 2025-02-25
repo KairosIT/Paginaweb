216a5f772dcd94df6e08da5988b3ff9f85d42b05
KairosIT <carlos.alferez@kairosit.com.co>
2025-02-25 01:56:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.getElementById("menu-btn");
+    const navMenu = document.getElementById("nav-menu");
 
+    menuBtn.addEventListener("click", function () {
+        navMenu.classList.toggle("active"); // 🔹 Activa o desactiva el menú
+    });
+});
